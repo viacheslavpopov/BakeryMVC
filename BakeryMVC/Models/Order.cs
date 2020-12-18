9cd4daaf0e77b4caa6b644b94d7b7d6866268e3f
ilromali <iliashenkoroman@gmail.com>
2020-12-18 10:26:47 -0800

    Order & vendor constructor outlines

--- BakeryMVC/Models/Order.cs
+++ BakeryMVC/Models/Order.cs
@@ -6,5 +6,17 @@ namespace BakeryMVC.Models
 	public class Order
 	{
 	//the title, the description, the price, the date, and any other properties you would like to include
+  public string Description { get; set; }
+  public int Price { get; set; }
+  public int Date { get; set; } 
+  public int Id { get; }
+
+  private static List<Order> _instances = new List<Order> {};
+
+  public Order(string description)
+  {
+
+  }
+
 	}
 }
--- BakeryMVC/Models/Vendor.cs
+++ BakeryMVC/Models/Vendor.cs
@@ -6,5 +6,17 @@ namespace BakeryMVC.Models
 	public class Vendor
 	{
 	//vendor's name, a description of the vendor, a List of Orders belonging to the vendor, and any other properties you would like to include.
+  private static List<Vendor> _instances = new List<Vendor> {};
+
+  public Vendor(string vendorName, string vendorType, string vendorOrders)
+  {
+    // VendorName = vendorName;
+    // VendorType = vendorType;
+    // VenderOrders = vendorOrders;
+    _instances.Add(this);
+    // Id = _instances.Count;
+    // vendorOrders = new List<Order>{};
+    //, int vendorInvoice
+  }
 	}
 }
