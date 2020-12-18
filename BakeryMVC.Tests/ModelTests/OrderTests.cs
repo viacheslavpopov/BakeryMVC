6d35fd49080264088ea937b218822ad5dcf63423
ilromali <iliashenkoroman@gmail.com>
2020-12-18 14:53:04 -0800

    Add basic order info & purposely pass corder constructor test

---
+++ BakeryMVC.Tests/ModelTests/OrderTests.cs
@@ -0,0 +1,111 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BakeryMVC.Models; 
+using System;
+
+namespace BakeryMVC.Test
+{
+  [TestClass]
+  public class OrderTests //: IDisposable
+  {
+
+    // public void Dispose()
+    // {
+    //   Order.ClearAll();
+    // }
+
+    [TestMethod]
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
+    {
+      Order newOrder = new Order("test");
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+
+    // [TestMethod]
+    // public void GetId_ItemsInstantiateWithAnIdAndGetterReturns_Int()
+    // {
+    //   //Arrange
+    //   string description = "Walk the dog.";
+    //   Item newItem = new Item(description);
+
+    //   //Act
+    //   int result = newItem.Id;
+
+    //   //Assert
+    //   Assert.AreEqual(1, result);
+    // }
+
+    // [TestMethod]
+    // public void Find_ReturnsCorrectItem_Item()
+    // {
+    //   //Arrange
+    //   string description01 = "Walk the dog";
+    //   string description02 = "Wash the dishes";
+    //   Item newItem1 = new Item(description01);
+    //   Item newItem2 = new Item(description02);
+
+    //   //Act
+    //   Item result = Item.Find(2);
+
+    //   //Assert
+    //   Assert.AreEqual(newItem2, result);
+    // }
+
+    // [TestMethod]
+    // public void GetDescription_ReturnsDescription_String()
+    // {
+    //   //Arrange
+    //   string description = "Walk the dog.";//creates instance
+    //   Item newItem = new Item(description); // pass new instance through Item to add to list
+    //   //Act
+    //   string result = newItem.Description; //calls on new item added to list
+    //   //Assert
+    //   Assert.AreEqual(description, result); //tests that expected result matches description of new item
+    // }
+    
+    // [TestMethod]
+    // public void SetDescription_SetDescription_String()
+    // {
+    //   //Arrange
+    //   string description = "Walk the dog.";
+    //   Item newItem = new Item(description);
+
+    //   //Act
+    //   string updatedDescription = "Do the dishes";
+    //   newItem.Description = updatedDescription;
+    //   string result = newItem.Description;
+
+    //   //Assert
+    //   Assert.AreEqual(updatedDescription, result);
+    // }
+
+    // [TestMethod]
+    // public void GetAll_ReturnsEmptyList_ItemList()
+    // {
+    //   // Arrange
+    //   List<Item> newList = new List<Item> { };
+
+    //   //Act
+    //   List<Item> result = Item.GetAll();
+
+    //   //Asset
+    //   CollectionAssert.AreEqual(newList, result);
+    // }
+
+    // [TestMethod]
+    // public void GetAll_ReturnsItems_ItemList()
+    // {
+    //   //Arrange
+    //   string description01 = "Walk the dog";
+    //   string description02 = "Wash the dishes";
+    //   Item newItem1 = new Item(description01);
+    //   Item newItem2 = new Item(description02);
+    //   List<Item> newList = new List<Item> { newItem1, newItem2 };
+
+    //   //Act
+    //   List<Item> result = Item.GetAll();
+      
+    //   //Assert
+    //   CollectionAssert.AreEqual(newList, result);
+    // }
+  }
+}
--- BakeryMVC/Models/Order.cs
+++ BakeryMVC/Models/Order.cs
@@ -6,15 +6,17 @@ namespace BakeryMVC.Models
 	public class Order
 	{//the title --> Vendor X Order No., the description( the price, the date, )
   private static List<Order> _instances = new List<Order> {};
+  public int Id { get; } // do not add set;
   public string Description { get; set; } //what order is for (event,etc) 
-  public int Price { get; set; } // total price // stretch individual line items for nested class orders & prices
-  public int Date { get; set; } // month, day, year
-  public int Id { get; set; }
-  public string OrderType { get; set; } //wholesale or retail
+  // public int Price { get; set; } // total price // stretch individual line items for nested class orders & prices
+  // public int Date { get; set; } // month, day, year
+  // public string OrderType { get; set; } //wholesale or retail
 
   public Order(string description)
   {
-
+    Description = description;
+    _instances.Add(this);
+    //Id = _instances.Count;
   }
 
 	}
