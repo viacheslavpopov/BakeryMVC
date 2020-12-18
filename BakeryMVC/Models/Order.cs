e329e8ae4693229a7ea19a29aefb0b0394e52e8e
ilromali <iliashenkoroman@gmail.com>
2020-12-18 08:48:57 -0800

    Add order and vendor files

---
+++ BakeryMVC/Models/Order.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using System;
+
+namespace BakeryMVC.Models
+{
+	public class Order
+	{
+	//methods here
+	}
+}
---
+++ BakeryMVC/Models/Vendor.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using System;
+
+namespace BakeryMVC.Models
+{
+	public class Vendor
+	{
+	//methods here
+	}
+}
